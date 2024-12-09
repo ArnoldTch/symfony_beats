0dec8a6653e5d068634a387997b4399e041af7db
Arnold TCHIASSO <arnold.tchiasso@eemi.com>
2024-12-09 12:19:20 +0100

    09/12/24 matin finir page de composition et contacts

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Composition;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+        
+    }
+
+    #[Route('/compositions', name: 'app_compositions')]
+    public function compositions(EntityManagerInterface $em): Response
+    {
+        // Récupérer les compositions depuis la base de données
+        $compositions = $em->getRepository(Composition::class)->findAll();
+
+        return $this->render('home/compositions.html.twig', [
+            'compositions' => $compositions,
+        ]);
+    }
+
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig');
+    }
+
+    
+}
